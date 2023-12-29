0c60bd63ecdf3d14e276fc0fd9afb689e92cc3a6
AbseLY <abdaslam13.2.2001@gmail.com>
2023-12-29 21:56:22 +0200

    gitting ready
    removed some files added components folder added Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+
+function Navbar() {
+    return (
+        <div className="
+        w-full
+         
+        ">
+            <h1 className="text-white">
+React
+            </h1>
+            <div className="navbar-menu">
+          <ul className="text-white flex 
+        flex-row-reverse">
+            <li>
+              <a href="#" className="p-6">Home</a>
+            </li>
+            <li>
+              <a href="#" className="p-4">About</a>
+            </li>
+          </ul>
+        </div>
+        </div>
+        
+    )
+                
+}
+
+export default Navbar
